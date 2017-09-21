618b8fee1cd300ef8310ca1cf40622ab013ed3ac
Shanaire Blythe <shanaire-blythe@hotmail.com>
2017-09-21 10:31:43 +0100

    Added Bug report file and code updated

--- ButtonPress.cs
+++ ButtonPress.cs
@@ -15,9 +15,13 @@ public class ButtonPress : MonoBehaviour
     public GameObject CamSetup;
     private CameraManager CamMan_1;
 
-    //Setting Connection to true when the function is ran
+    // Setting Connection to true when the function is ran
     private DeactivatePan ConditionTog;
     public GameObject FloorPlanPanel;
+
+    // Fading script
+    public FadeToCamPos FadeToCam;
+
     //_________________________________________________________________________________________//
 
 
@@ -33,7 +37,9 @@ public class ButtonPress : MonoBehaviour
         if (tag == "Room1")
         {
             // Add an if statement for each of the separate rooms
+            FadeToCam.RoomFadeOUT();
             CamMan_1.CameraCount = 11;
+            FadeToCam.RoomFadeIN();
             FloorPlanPanel.SetActive(false);
 
             Debug.Log("Change Room Test");
@@ -43,7 +49,9 @@ public class ButtonPress : MonoBehaviour
         else if (tag == "Room2")
         {
             // Add an if statement for each of the separate rooms
+            FadeToCam.RoomFadeOUT();
             CamMan_1.CameraCount = 12;
+            FadeToCam.RoomFadeIN();
             FloorPlanPanel.SetActive(false);
 
             Debug.Log("Change Room Test");
@@ -53,7 +61,45 @@ public class ButtonPress : MonoBehaviour
         else if (tag == "Room3")
         {
             // Add an if statement for each of the separate rooms
+            FadeToCam.RoomFadeOUT();
             CamMan_1.CameraCount = 13;
+            FadeToCam.RoomFadeIN();
+            FloorPlanPanel.SetActive(false);
+
+            Debug.Log("Change Room Test");
+
+            ConditionTog.condition = false;
+        }
+        else if (tag == "Room4")
+        {
+            // Add an if statement for each of the separate rooms
+            FadeToCam.RoomFadeOUT();
+            CamMan_1.CameraCount = 14;
+            FadeToCam.RoomFadeOUT();
+            FloorPlanPanel.SetActive(false);
+
+            Debug.Log("Change Room Test");
+
+            ConditionTog.condition = false;
+        }
+        else if (tag == "Room5")
+        {
+            // Add an if statement for each of the separate rooms
+            FadeToCam.RoomFadeOUT();
+            CamMan_1.CameraCount = 15;
+            FadeToCam.RoomFadeOUT();
+            FloorPlanPanel.SetActive(false);
+
+            Debug.Log("Change Room Test");
+
+            ConditionTog.condition = false;
+        }
+        else if (tag == "Room6")
+        {
+            // Add an if statement for each of the separate rooms
+            FadeToCam.RoomFadeOUT();
+            CamMan_1.CameraCount = 16;
+            FadeToCam.RoomFadeOUT();
             FloorPlanPanel.SetActive(false);
 
             Debug.Log("Change Room Test");
