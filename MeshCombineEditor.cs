924b7959d464c3f4e521992da6c955a9271b579d
Shanaire Blythe <shanaire-blythe@hotmail.com>
2017-09-25 17:45:43 +0100

    update

--- MeshCombineEditor.cs
+++ MeshCombineEditor.cs
@@ -11,15 +11,22 @@ public class MeshCombineEditor : Editor
     public override void OnInspectorGUI()
     {
         MeshCombiner mc = target as MeshCombiner;
-
+/*
         if (GUILayout.Button("Normal Combine Meshes"))
         {
             mc.CombineMeshes();
-        }
+        }*/
         if (GUILayout.Button("Multiple Materials Combination"))
         {
             mc.AdvanceMeshCombine();
         }
+        // Gameobject to create the meshes into.
+
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("ParentMeshes"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("ParentTransform"), true);
+        serializedObject.ApplyModifiedProperties();
+
     }
 }
 
--- MeshCombiner.cs
+++ MeshCombiner.cs
@@ -26,9 +26,14 @@ public class MeshCombiner : MonoBehaviour
     [System.NonSerialized]
     public int vertexLimit = 30000;
 
+    public Transform ParentTransform;
+    public GameObject ParentMeshes;
+
 
     public void AdvanceMeshCombine()
     {
+        int vertexCount = 0;
+
         // This is getting the MeshFilter component of the parent gameobject and storing it in myMeshFilter
         MeshFilter myMeshFilter = new MeshFilter();
         myMeshFilter = GetComponent<MeshFilter>();
@@ -73,16 +78,19 @@ public class MeshCombiner : MonoBehaviour
             // This will also go through all the children submeshes to see if anyof them have the materials that we are currently trying to find.
             // This basically checking all the materials that are already collected with the materials that are in the one the mesh filter to match it, so that it can be applied.
             List<CombineInstance> combiners = new List<CombineInstance>();
-            foreach (MeshFilter item_2 in filters)
+
+            for (int item_2 = 0; item_2 < filters.Length; item_2++)
             {
-                MeshRenderer renderer = item_2.GetComponent<MeshRenderer>(); // We have to use the mesh renderer to check if the childern item from the mesh filter are using the same material that we are trying to find.
+                MeshRenderer renderer = filters[item_2].GetComponent<MeshRenderer>(); // We have to use the mesh renderer to check if the childern item from the mesh filter are using the same material that we are trying to find.
                 if (renderer == null)
                 {
-                    Debug.Log(item_2.name + "No MeshRenderer");
+                    Debug.Log(filters[item_2].name + "No MeshRenderer");
                     continue;
                 }
                 // Now lets create an instance of all the materials that are on the current meshrenderer and add it to the array.
                 Material[] localMaterials = renderer.sharedMaterials; // Adding all materials on that are availabel through the mesh rendenerers to the array
+
+
                 for (int materialIndex = 0; materialIndex < localMaterials.Length; materialIndex++)
                 {
                     if (localMaterials[materialIndex] != item)
@@ -92,9 +100,9 @@ public class MeshCombiner : MonoBehaviour
                     // But if the material is in the list and it is the material we are looking for, the add it to the combineinstance
                     CombineInstance ci = new CombineInstance
                     {
-                        mesh = item_2.sharedMesh,
+                        mesh = filters[item_2].sharedMesh,
                         subMeshIndex = materialIndex,
-                        transform = item_2.transform.localToWorldMatrix
+                        transform = filters[item_2].transform.localToWorldMatrix
                     };
                     combiners.Add(ci);
                 }
@@ -103,20 +111,48 @@ public class MeshCombiner : MonoBehaviour
                 Mesh mesh = new Mesh();
                 mesh.CombineMeshes(combiners.ToArray(), true);
 
-                Debug.Log("Vert Count " + mesh.vertexCount);
-                
                 submeshes.Add(mesh);
 
 
                 // THINK ABOUT HAVING THE OBJECT CREATE A NEW OBJECT FOR EACH ITERATION OF THE MESH THAT IT IS DOING
 
+                vertexCount += mesh.vertexCount;
+                Debug.Log("Vert Count " + vertexCount);
+
+                if (vertexCount > vertexLimit)
+                {
+                    // change the foreach loop to a for loop, then i can do i -1
+                    item_2 -= 1;
+                    // Remove the last mesh we added to the list as the limit was exceeded.
+                    submeshes.RemoveAt(submeshes.Count - 1);
+
+                    // Instantiate an new gameObject withthe current geometry
+                    CreateCombinedMesh(submeshes, ParentMeshes);
+
+                    // set number of verts back to 0;
+                    vertexCount = 0;
+                    submeshes.Clear();
+
+                }
+
 
             }
         }
-        // Now we can create our final combiners that will combine all the submeshes into one mesh
 
+        CreateCombinedMesh(submeshes, ParentMeshes);
+
+
+        /* [BREAK TO REJOIN IF NECESSARY]*/
+    }
+
+    // Function to create the combined meshes;
+    public void CreateCombinedMesh(List<Mesh> ListOfMeshes, GameObject ObjToSaveTo)
+    {
+        // Create an instance of a new mesh to hold the combined meshes
         List<CombineInstance> finalCombiners = new List<CombineInstance>(); // A list to hold all the final set of combine instances that is to be placed into the final mesh
-        foreach (Mesh item in submeshes)
+
+
+        foreach (Mesh item in ListOfMeshes)
         {
             CombineInstance ci = new CombineInstance
             {
@@ -124,29 +160,74 @@ public class MeshCombiner : MonoBehaviour
                 subMeshIndex = 0,
                 transform = Matrix4x4.identity
             };
-           
+
             finalCombiners.Add(ci);
         }
-        Mesh finalMesh_2 = new Mesh();
-        finalMesh_2.CombineMeshes(finalCombiners.ToArray(), false);
+
+        Mesh newMesh = new Mesh();
+
+        newMesh.CombineMeshes(finalCombiners.ToArray(),false);
+
+        // Instantiate a new game object to hold the combined meshes.
+        GameObject _newGameObject = Instantiate(ObjToSaveTo, Vector3.zero, Quaternion.identity) as GameObject;
+
+        _newGameObject.transform.parent = ParentTransform;
+        // Add the mesh to the mesh filter of the new game object;
+        _newGameObject.GetComponent<MeshFilter>().sharedMesh = newMesh;
+    }
+}
 
 
-        myMeshFilter.sharedMesh = finalMesh_2;
-
-        //Debug.Log("Final mesh has" + submeshes.Count + " materials.");
 
 
-        transform.rotation = oldRot;
-        transform.position = oldPos;
 
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            transform.GetChild(i).gameObject.SetActive(false);
+
+
+/*              [BREAK FOR TESTING]
+            // Now we can create our final combiners that will combine all the submeshes into one mesh
+
+            List<CombineInstance> finalCombiners = new List<CombineInstance>(); // A list to hold all the final set of combine instances that is to be placed into the final mesh
+            foreach (Mesh item in submeshes)
+            {
+                CombineInstance ci = new CombineInstance
+                {
+                    mesh = item,
+                    subMeshIndex = 0,
+                    transform = Matrix4x4.identity
+                };
+
+                finalCombiners.Add(ci);
+            }
+            Mesh finalMesh_2 = new Mesh();
+            finalMesh_2.CombineMeshes(finalCombiners.ToArray(), false);
+
+
+            myMeshFilter.sharedMesh = finalMesh_2;
+
+            //Debug.Log("Final mesh has" + submeshes.Count + " materials.");
+
+
+            transform.rotation = oldRot;
+            transform.position = oldPos;
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+
         }
 
-    }
+        */
 
 
+
+
+
+
+
+
+
+    /*
 
     public void CombineMeshes()
     {
@@ -207,3 +288,4 @@ public class MeshCombiner : MonoBehaviour
 
     }
 }
+*/
